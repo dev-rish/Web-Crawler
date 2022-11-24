27a9e37cd69534cc1a1917f1016357f9c46058c5
Rishabh Singh <rishabh.singh@mapsted.com>
2022-11-24 13:06:33 +0530

    impl. get job route

--- queue/routes/dashboard.js
+++ queue/routes/dashboard.js
@@ -1,11 +1,27 @@
 const { Client } = require("pg");
+const { Router } = require("express");
 
 const boss = require("../boss");
 const { CRAWL_JOB_QUEUE_NAME } = require("../constants");
+const { getProducts } = require("../database");
 const { formatProduct } = require("../utils");
 
 const PRODUCT_EVENT = "PRODUCT_EVENT";
 const JOB_EVENT = "JOB_EVENT";
+
+const router = Router();
+
+router.get("/products", async (req, res) => {
+  try {
+    let products = await getProducts();
+    products = products.map(formatProduct);
+
+    res.send(formatResponse(true, products));
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+});
 
 const registerDashboardHandlers = (io, socket) => {
   boss.on("monitor-states", ({ queues }) => {
@@ -26,4 +42,5 @@ const registerDashboardHandlers = (io, socket) => {
 
 module.exports = {
   registerDashboardHandlers,
+  dashboardRoutes: router,
 };
