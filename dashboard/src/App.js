9eff07908764bffcb41b69eb4723caf52966d8b8
Rishabh Singh <rishabh.singh@mapsted.com>
2022-11-25 16:29:10 +0530

    install deps for dashboard & setup web socket

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:4000");
 
 function App() {
-  return (
-    <div>Dashboard</div>
-  );
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+  }, []);
+
+  return <div>Hi {isConnected}</div>;
 }
 
 export default App;
